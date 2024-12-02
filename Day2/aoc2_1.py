277a8d8b868ab0949660ae16ebf0f58b877d9d1a
Himanshu Dias <himanshudias2004@gmail.com>
2024-12-02 22:47:50 +0000

    part 1 day 2 done

--- Day2/aoc2_1.py
+++ Day2/aoc2_1.py
@@ -3,9 +3,33 @@ inp = open("Day2/input2.txt")
 safe_count = 0
 
 for line in inp:
-    arr = line.split(" ")
-    print(arr)
-     
-    st = int(arr[0])
-    for i in range (1,len(arr)):
-        if int(arr[i])
+    arr = list(map(int,line.strip().split()))
+ #   print(arr)
+    flag = True
+    if (arr[0]<arr[1]):
+        #increasing
+        for i in range(1,len(arr)):
+            if (arr[i-1] >= arr[i] or arr[i]-arr[i-1]>3):
+                flag = False
+                break
+        if flag:
+            safe_count = safe_count + 1
+    elif (arr[0]>arr[1]):
+        #decreasing
+        for i in range(1,len(arr)):
+            if (arr[i-1]<=arr[i] or arr[i-1]-arr[i]>3):
+                flag = False
+                break
+        if flag:
+            safe_count = safe_count + 1   
+
+    else:
+        #equals so not safe
+        pass
+
+
+
+
+ 
+
+print(safe_count)
